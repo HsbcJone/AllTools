3d85ffdc61fea84119bed01f5cb19ce42061b7d8
mengxiaopeng <xiaopengm@digitalgd.com.cn>
2024-06-07 15:03:38 +0800

    [+] 增加MYSQLPO

---
+++ FlinkTest/src/main/java/flink/test/po/MysqlPo.java
@@ -0,0 +1,74 @@
+package flink.test.po;
+
+public class MysqlPo {
+
+    private String hostName;
+    private int port;
+
+    private String databaseName;
+
+    private String userName;
+
+    private String password;
+
+    private String tableName;
+
+
+    private String passWord;
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String getHostName() {
+        return hostName;
+    }
+
+    public void setHostName(String hostName) {
+        this.hostName = hostName;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getDatabaseName() {
+        return databaseName;
+    }
+
+    public void setDatabaseName(String databaseName) {
+        this.databaseName = databaseName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
